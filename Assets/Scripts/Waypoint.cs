caf28542a72799416b1051928fedfb78daae2b9f
Soulgypsy79 <jackbates065@gfmailc.om>
2022-11-29 14:32:53 +0000

    Waypoint stuff

---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Waypoint : MonoBehaviour
+{
+    public Image questImage;
+    public Transform targetPosition;
+    public Text distanceToTarget;
+    public Camera cam;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float minX = questImage.GetPixelAdjustedRect().width / 2;
+        float maxX = Screen.width - minX;
+
+        float minY = questImage.GetPixelAdjustedRect().height / 2;
+        float maxY = Screen.width - minY;
+
+        Vector2 pos = cam.WorldToScreenPoint(targetPosition.position);
+
+        if (Vector3.Dot((targetPosition.position - transform.position), transform.forward) < 0)
+        {
+            //Target is behind camera
+            if (pos.x < Screen.width / 2)
+            {
+                pos.x = maxX;
+            }
+            else
+            {
+                pos.x = minX;
+            }
+        }
+
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+        questImage.transform.position = pos;
+        distanceToTarget.text = ((int)Vector3.Distance(targetPosition.position, transform.position)).ToString() + "m";
+    }
+}
