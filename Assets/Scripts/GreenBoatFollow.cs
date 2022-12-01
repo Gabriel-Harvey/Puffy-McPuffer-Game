b7cda0a22976dea5ad7b1447e0bd5800bbbe5fc0
Soulgypsy79 <jackbates065@gfmailc.om>
2022-12-01 16:20:08 +0000

    Dialogue system with fully functioning escort quest

--- Assets/Scripts/GreenBoatFollow.cs
+++ Assets/Scripts/GreenBoatFollow.cs
@@ -7,7 +7,12 @@ public class GreenBoatFollow : MonoBehaviour
     public float followerSpeed = 10f;
     public float followerTurningSpeed = 10f;
     public Transform transformPlayer;
+    public bool followAllowed = false;
     public bool reachedPoint = false;
+    public GameObject interactImage;
+
+    [SerializeField]
+    private KeyCode followButton;
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +23,26 @@ public class GreenBoatFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(transform.position, transformPlayer.position) < 30 && Vector3.Distance(transform.position, transformPlayer.position) > 10)
+        if (followAllowed == true)
         {
-            if (reachedPoint == false)
+            if (Vector3.Distance(transform.position, transformPlayer.position) < 30 && Vector3.Distance(transform.position, transformPlayer.position) > 10)
             {
-                Follow();
+                if (reachedPoint == false)
+                {
+                    Follow();
+                }
             }
+        }
+        if (Vector3.Distance(transform.position, transformPlayer.position) < 20 && followAllowed == false)
+        {
+            interactImage.SetActive(true);
+            if (Input.GetKeyDown(followButton))
+            {
+                interactImage.SetActive(false);
+                GetComponent<DialogueTrigger>().TriggerDialogue();
+                followAllowed = true;
+            }
+            
         }
     }
 
@@ -42,6 +61,7 @@ public class GreenBoatFollow : MonoBehaviour
     {
         if (other.tag == "Point")
         {
+            other.gameObject.GetComponent<DialogueTrigger>().TriggerDialogue();
             reachedPoint = true;
         }
     }
